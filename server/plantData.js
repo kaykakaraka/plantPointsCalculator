50465cd6aa6d2cde3deeb933552081c0449e69c4
kaykakaraka <23278323+kaykakaraka@users.noreply.github.com>
2024-02-06 16:25:18 +0000

    Created a PlantData object that will contain the information on plants until moved to a database

---
+++ server/plantData.js
@@ -0,0 +1,22 @@
+class plantData {
+  constructor() {
+    this.fruit = {
+
+    };
+    this.veg = {
+
+    }
+    this.wholegrains = {
+
+    }
+    this.nutsAndSeeds = {
+
+    }
+    this.legumes = {
+
+    }
+    this.herbsAndSpices = {
+
+    }
+  }
+}
